9c8da0f9c5d2b83930bea27b80e80a665f005298
Vitalii Shulhan <vitalii.shulhan@accenture.com>
2025-04-22 11:22:43 +0200

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    credentials: true,
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(3000);
 }
 bootstrap();
